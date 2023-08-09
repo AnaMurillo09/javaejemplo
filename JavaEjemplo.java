e27a3c07ee78694f8b81ab13de86aa1b2bae958c
AnaMurillo09 <ana.v.murillo@gmail.com>
2023-08-09 15:43:06 -0300

    Update JavaEjemplo.java

--- JavaEjemplo.java
+++ JavaEjemplo.java
@@ -5,6 +5,8 @@ public class JavaEjemplo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("hola");
+        int num = 4;
+        
     }
     
 }
